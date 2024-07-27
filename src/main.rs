f3c35fef785c880a04cdd75390211778b9061cad
sunjiang <sunjiang0018@gmail.com>
2024-07-27 15:30:08 +0800

    feature: add csv cli support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, rust!");
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+// rcli csv i input.csv -o output.json --header -d ','
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+
+    Ok(())
 }
---
+++ src/opts.rs
@@ -0,0 +1,38 @@
+use std::path::Path;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name ="rcil", version, author,about, long_about = None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(about = "Show CSV, or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    pub input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File does not exist")
+    }
+}
