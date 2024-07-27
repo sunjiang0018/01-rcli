d045029d0f430cf5d9724d8160cad725dff80cc4
sunjiang <sunjiang0018@gmail.com>
2024-07-27 20:05:43 +0800

    feature: add password generator

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use clap::Parser;
-use rcli::{process_csv, Opts, SubCommand};
+use rcli::{process_csv, process_gen_pass, Opts, SubCommand};
 
 // rcli csv i input.csv -o output.json --header -d ','
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
-    println!("{:?}", opts);
 
     match opts.cmd {
         SubCommand::Csv(opts) => {
@@ -15,6 +14,16 @@ fn main() -> anyhow::Result<()> {
                 format!("output.{}", opts.format)
             };
             process_csv(&opts.input, output, opts.format)?
+        }
+        SubCommand::GenPass(opts) => {
+            let password = process_gen_pass(
+                opts.length,
+                opts.uppercase,
+                opts.lowercase,
+                opts.number,
+                opts.symbol,
+            );
+            println!("Generate password: {}", password);
         }
     }
 
---
+++ src/process/gen_pass.rs
@@ -0,0 +1,44 @@
+use rand::seq::SliceRandom;
+
+const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
+const LOWER: &[u8] = b"abcdefghjklmnopqrstuvwxyz";
+const NUMBER: &[u8] = b"123456789";
+const SYMBOL: &[u8] = b"!@#$%^&*_";
+pub fn process_gen_pass(
+    length: u8,
+    upper: bool,
+    lower: bool,
+    number: bool,
+    symbol: bool,
+) -> String {
+    let mut rng = rand::thread_rng();
+    let mut password = Vec::new();
+    let mut chars = Vec::new();
+
+    if upper {
+        chars.extend_from_slice(UPPER);
+        password.push(*UPPER.choose(&mut rng).unwrap());
+    }
+
+    if lower {
+        chars.extend_from_slice(LOWER);
+        password.push(*LOWER.choose(&mut rng).unwrap());
+    }
+    if number {
+        chars.extend_from_slice(NUMBER);
+        password.push(*NUMBER.choose(&mut rng).unwrap());
+    }
+    if symbol {
+        chars.extend_from_slice(SYMBOL);
+        password.push(*SYMBOL.choose(&mut rng).unwrap());
+    }
+
+    for _ in 0..(length - password.len() as u8) {
+        let c = chars.choose(&mut rng).unwrap();
+        password.push(*c);
+    }
+
+    password.shuffle(&mut rng);
+
+    String::from_utf8(password).unwrap()
+}
